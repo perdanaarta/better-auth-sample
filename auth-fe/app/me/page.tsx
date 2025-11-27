36fa18c2390a076179be4df0f545ad1395634f7b
artagny <itho.dev11.gny@gmail.com>
2025-11-27 10:52:31 +0700

    fetch me data

--- auth-fe/app/me/page.tsx
+++ auth-fe/app/me/page.tsx
@@ -2,28 +2,33 @@
 
 import auth from "@/lib/auth";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-    const router = useRouter();
-    const { data: session, isPending } = auth.useSession();
+    const [data, setData] = useState<{ user: {name: string, email: string} } | null>(null);
 
-    if (!isPending && !session) {
-        router.replace("/auth");
-        return null;
-    }
-
-    if (isPending) {
-        return <p>Loading...</p>;
-    }
-
-    var user = session!.user;
+    useEffect(() => {
+        fetch("http://localhost:5000/me", {
+            credentials: "include",
+        })
+            .then(r => r.json())
+            .then(r => {
+                setData(r);
+                console.log(r) 
+            });
+    }, []);
 
     return (
         <main>
-            <h1>Dashboard</h1>
-            <p>Welcome, {user.email}</p>
-            <p>{user.name}</p>
-            <p>{user.id}</p>
+            <h1>Data</h1>
+
+            {!data ? (
+                <p>Loading...</p>
+            ) : data.user ? (
+                <p>Welcome, {data.user.name} </p>
+            ) : (
+                <p>Not logged in</p>
+            )}
         </main>
-    )
+    );
 }
