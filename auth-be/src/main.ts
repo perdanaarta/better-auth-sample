c3d4a5384620d5e025d0875ba7f5124ce4cc5213
perdanaarta <me.perdanaarta@gmail.com>
2025-12-01 20:34:32 +0700

    cookie run 5

--- auth-be/src/main.ts
+++ auth-be/src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   });
 
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(process.env.PORT ?? 5000, "0.0.0.0");
 }
 bootstrap();
